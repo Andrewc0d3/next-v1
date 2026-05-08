40fb5435939fc9bb7f575579bcfe9ecfaf179ac9
Andrewc0d3 <davidsanchez102502@gmail.com>
2026-05-08 14:05:10 -0500

    next(sin_error)

--- app/uno/props.tsx
+++ app/uno/props.tsx
@@ -1,4 +1,10 @@
-export default function Props({ nombre }){
+import { ReactNode } from 'react';
+
+type PropsProps = {
+    nombre?: ReactNode;
+};
+
+export default function Props({ nombre }: PropsProps) {
     return (
         <div>
             {nombre}
