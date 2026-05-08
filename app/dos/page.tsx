08015b7a422f20caff85c189c0d1d803bc23d06b
Andrewc0d3 <davidsanchez102502@gmail.com>
2026-05-08 13:58:36 -0500

    next(hijo,prop,page)

---
+++ app/dos/page.tsx
@@ -0,0 +1,10 @@
+import Hijo from "./hijo"
+export default function Uno(){
+    return(
+        <div>
+            <Hijo>
+                dos
+            </Hijo>
+        </div>
+    )
+}
---
+++ app/uno/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import Props from './props'
+export default function Uno(){
+    return(
+        <div>
+            Uno
+            <Link href= "/">Boton Home</Link>
+            <Link href= "/uno">Boton Uno</Link>
+            <Link href= "/dos">Boton Dos</Link>
+
+            <Props nombre="este es un props"></Props>
+        </div>
+    )
+}
